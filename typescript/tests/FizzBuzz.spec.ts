45bf640d72a3bc66b3dc61615d383b88a071744d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 21:23:59 +0200

    RED: should return 2 as String when a 2 is given

--- typescript/tests/FizzBuzz.spec.ts
+++ typescript/tests/FizzBuzz.spec.ts
@@ -1,9 +1,15 @@
 import { FizzBuzz } from '../src/FizzBuzz'
 
 describe('FizzBuzz', () => {
-  it('should return 1 as String when a 1 is given', () => {
+    it('should return 1 as String when a 1 is given', () => {
     const example = new FizzBuzz()
 
     expect(example.calculate(1)).toBe("1")
-  })
+    })
+
+    it('should return 2 as String when a 2 is given', () => {
+        const example = new FizzBuzz()
+
+        expect(example.calculate(2)).toBe("2")
+    })
 })
