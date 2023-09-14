1d1dfba7ce4d5ecb4936aeca800c430093cd2aca
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2023-09-14 21:26:40 +0200

    RED: should return 4 as String when a 4 is given

--- typescript/src/FizzBuzz.ts
+++ typescript/src/FizzBuzz.ts
@@ -1,5 +1,8 @@
 export class FizzBuzz {
   calculate(number: Number): String {
+    if (number === 2) {
+      return "2"
+    }
     return "1"
   }
 }
--- typescript/tests/FizzBuzz.spec.ts
+++ typescript/tests/FizzBuzz.spec.ts
@@ -1,15 +1,21 @@
 import { FizzBuzz } from '../src/FizzBuzz'
 
 describe('FizzBuzz', () => {
-    it('should return 1 as String when a 1 is given', () => {
+  it('should return 1 as String when a 1 is given', () => {
+  const example = new FizzBuzz()
+
+  expect(example.calculate(1)).toBe("1")
+  })
+
+  it('should return 2 as String when a 2 is given', () => {
+      const example = new FizzBuzz()
+
+      expect(example.calculate(2)).toBe("2")
+  })
+
+  it('should return 4 as String when a 4 is given', () => {
     const example = new FizzBuzz()
 
-    expect(example.calculate(1)).toBe("1")
-    })
-
-    it('should return 2 as String when a 2 is given', () => {
-        const example = new FizzBuzz()
-
-        expect(example.calculate(2)).toBe("2")
-    })
+    expect(example.calculate(4)).toBe("4")
+  })
 })
